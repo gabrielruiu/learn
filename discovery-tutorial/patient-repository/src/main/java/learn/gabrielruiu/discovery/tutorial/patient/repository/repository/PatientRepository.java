b51d9a8030ef14f1096b1ba80630462cc4d65d4c
Gabriel Ruiu <gabriel.ruiu@mail.com>
2016-06-08 22:57:06 +0200

    add @Param annotations to PatientRepository

--- discovery-tutorial/patient-repository/src/main/java/learn/gabrielruiu/discovery/tutorial/patient/repository/repository/PatientRepository.java
+++ discovery-tutorial/patient-repository/src/main/java/learn/gabrielruiu/discovery/tutorial/patient/repository/repository/PatientRepository.java
@@ -2,11 +2,12 @@ package learn.gabrielruiu.discovery.tutorial.patient.repository.repository;
 
 import learn.gabrielruiu.discovery.tutorial.patient.repository.model.Patient;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 
 /**
  * @author Gabriel Mihai Ruiu (gabriel.ruiu@mail.com)
  */
 public interface PatientRepository extends PagingAndSortingRepository<Patient, Long> {
 
-    Patient findByFirstNameAndLastName(String firstName, String lastName);
+    Patient findByFirstNameAndLastName(@Param("firstName") String firstName, @Param("lastName") String lastName);
 }
