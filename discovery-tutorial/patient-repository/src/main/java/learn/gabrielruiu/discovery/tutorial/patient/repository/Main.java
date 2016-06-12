e5571acf25773943fc2d482c293a39862edc3799
Gabriel Ruiu <gabriel.ruiu@mail.com>
2016-06-12 12:47:34 +0200

    set default properties when starting up patient-repository

--- discovery-tutorial/patient-repository/src/main/java/learn/gabrielruiu/discovery/tutorial/patient/repository/Main.java
+++ discovery-tutorial/patient-repository/src/main/java/learn/gabrielruiu/discovery/tutorial/patient/repository/Main.java
@@ -3,6 +3,9 @@ package learn.gabrielruiu.discovery.tutorial.patient.repository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author Gabriel Mihai Ruiu (gabriel.ruiu@mail.com)
  */
@@ -10,6 +13,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Main {
 
     public static void main(String[] args) {
-        SpringApplication.run(Main.class, args);
+        SpringApplication springApplication = new SpringApplication(Main.class);
+        springApplication.setDefaultProperties(defaultProperties());
+        springApplication.run(args);
+    }
+
+    private static Map<String, Object> defaultProperties() {
+        Map<String, Object> defaultProperties = new HashMap<>();
+        defaultProperties.put("spring.profiles.active", "in-memory");
+        return defaultProperties;
     }
 }
