cd37dcbc9f7bc74a3d9640a440c9d350e351f9f4
Gabriel Ruiu <gabriel.ruiu@mail.com>
2016-06-13 20:43:44 +0200

    prepare Dockerfile for custom mysql container

---
+++ discovery-tutorial/patient-db/schema.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE IF NOT EXISTS `patient-db`;
+
+CREATE TABLE patient (
+  id INT NOT NULL AUTO_INCREMENT,
+  created_date DATETIME NOT NULL,
+  last_changed_date DATETIME NOT NULL,
+  first_name VARCHAR(255) NOT NULL,
+  last_name VARCHAR(255) NOT NULL,
+  birth_date DATETIME NOT NULL,
+  gender VARCHAR(20) NOT NULL,
+  PRIMARY KEY (id)
+);
