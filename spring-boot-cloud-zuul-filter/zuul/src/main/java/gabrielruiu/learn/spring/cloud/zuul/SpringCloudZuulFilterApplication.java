2f55804117ac20d463870348bf2e1b13cdab8e85
Gabriel Ruiu <gabriel.ruiu@mail.com>
2016-07-07 20:01:06 +0200

    spring-boot-cloud-zuul-filter - initial commit

---
+++ spring-boot-cloud-zuul-filter/zuul/src/main/java/gabrielruiu/learn/spring/cloud/zuul/SpringCloudZuulFilterApplication.java
@@ -0,0 +1,12 @@
+package gabrielruiu.learn.spring.cloud.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudZuulFilterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudZuulFilterApplication.class, args);
+	}
+}
---
+++ spring-boot-cloud-zuul-filter/zuul/src/test/java/gabrielruiu/learn/spring/cloud/zuul/SpringCloudZuulFilterApplicationTests.java
@@ -0,0 +1,16 @@
+package gabrielruiu.learn.spring.cloud.zuul;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = SpringCloudZuulFilterApplication.class)
+public class SpringCloudZuulFilterApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
