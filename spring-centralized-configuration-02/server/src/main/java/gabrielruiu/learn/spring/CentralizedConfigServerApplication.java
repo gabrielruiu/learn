51cb2f6ff0bae973137569690bac3d4db1af5672
Gabriel Ruiu <gabriel.ruiu@mail.com>
2016-07-18 22:28:46 +0200

    spring-centralized-configuration-02 - add sources for config server

---
+++ spring-centralized-configuration-02/server/src/main/java/gabrielruiu/learn/spring/CentralizedConfigServerApplication.java
@@ -0,0 +1,12 @@
+package gabrielruiu.learn.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentralizedConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CentralizedConfigServerApplication.class, args);
+	}
+}
