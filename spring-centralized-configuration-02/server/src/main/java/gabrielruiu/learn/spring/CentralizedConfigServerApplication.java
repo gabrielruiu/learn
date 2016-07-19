733a89226ba40c453a0a313eb9b4f4f6e465cb15
Gabriel Ruiu <gabriel.ruiu@mail.com>
2016-07-19 20:28:06 +0200

    spring-centralized-configuration-02 - finish configuring server

--- spring-centralized-configuration-02/server/src/main/java/gabrielruiu/learn/spring/CentralizedConfigServerApplication.java
+++ spring-centralized-configuration-02/server/src/main/java/gabrielruiu/learn/spring/CentralizedConfigServerApplication.java
@@ -2,7 +2,9 @@ package gabrielruiu.learn.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
 @SpringBootApplication
 public class CentralizedConfigServerApplication {
 
